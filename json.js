1eafc7a01d04442730ac7a98ef9cecc07f402971
lepurini <Roberto Lepuri@cba.local>
2022-06-08 16:30:54 +0200

    mercolediOtto

--- json.js
+++ json.js
@@ -1,6 +1,5 @@
 const fs = require('fs');
 const express = require('express');
-//const { rows } = require('pg/lib/defaults');
 const knex = require('knex')({
     client: 'pg',
     connection: {
@@ -13,7 +12,6 @@ const knex = require('knex')({
 });
 
 const app = express();
-
 const host = 'localhost';
 const port = 8000;
 
@@ -32,10 +30,9 @@ app.get('/', (req, res) => {
     res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, Content-Type, Accept, Accept-Language, Origin, User-Agent');
     res.setHeader("Content-Type", "application/json");
 
-
     res.writeHead(200);
 
-    knex.select("*").from("domande_questionario").then((rows1) => {   //domande_questionario dipendenti
+    knex.select("*").from("domande_questionario").then((rows1) => {
         rows1 = aggiustaStringa(rows1);
 
         knex.select("*").from("dipendenti").then((rows2) => {
@@ -53,14 +50,34 @@ app.get('/', (req, res) => {
 app.post("/", (req, res) => {
     let body = "";
 
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,OPTIONS,POST');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, Content-Type, Accept, Accept-Language, Origin, User-Agent');
+    res.setHeader("Content-Type", "application/json");
     res.setHeader('Access-Control-Allow-Headers', 'Accept,Accept-Language, Content - Language, Content - Type');
 
     req.on('data', chunk => {
-        body += chunk;
+        //console.log(chunk);
+        //body += chunk;
+        body = body + chunk;
     });
 
     req.on('end', () => {
+        body = JSON.parse(body);
         console.log(body);
+        knex.insert({ data: body.data, tipo: body.tipo, id_dipendente: body.id_dipendente, id_valutatore: null }).into("valutazione_te").returning('id').then((result) => {
+            let id = result[0].id;
+            body.risposteDomande.forEach(element => {
+                knex.insert({ id_te: id, id_domanda: element.id_domanda, note: element.nota, punteggio: element.punteggio }).into("valutazione_de").then(() => {
+                });
+            });
+            body.domFinali.forEach(element => {
+                knex.insert({ id_te: id, id_domanda: element.id_domanda, note: element.nota, punteggio: null }).into("valutazione_de").then(() => {
+                });
+            });
+        });
+        // res.writeHead(200);
+        // res.end('ricevuto');
         res.writeHead(200);
         res.end('ricevuto');
     });
@@ -75,7 +92,6 @@ app.listen(port, host, () => {
             console.log(err);
             return;
         }
-        //console.log(data);
         fileModello = data;
     });
 });
